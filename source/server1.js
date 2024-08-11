2144b2726a6510f2659707f15e2bb498d4fec1c2
nikhithaathota <nikhithaathota@gmail.com>
2024-08-11 18:37:47 +0530

    hii

--- source/server1.js
+++ source/server1.js
@@ -19,6 +19,16 @@ app.post('/insert', async(req, res) => {
     .catch((e)=>console.log(e))
 })
 
+app.post('/signup', async(req, res) => {
+    
+    await db.collection("ast").insertOne({Name:req.body.name,email: req.body.email,password : req.body.password})
+    .then((result)=>{
+        res.json(result)
+        console.log(result);
+    })
+    .catch((e)=>console.log(e))
+})
+
 app.post('/insertmany', async(req, res) => {
     await db.collection("ast").insertMany(req.body)
     .then((result)=>{
@@ -27,9 +37,58 @@ app.post('/insertmany', async(req, res) => {
     .catch((e)=>console.log(e))
 })
 
-
-connectToDB(() => {
-    app.listen(9000, () => {
-        console.log("server running at 9000");
+app.post('/forgetpassword', async(req, res) => {
+    await db.collection("login").findOne({Email:req.body.email})
+    .then((result)=>{
+        
+        res.json({message:"change password",values:result})
+        
     })
+    .catch((e)=>console.log(e))
 })
+app.post('/forgetpassword', async(req, res) => {
+    await db.collection("login").updateOne({Password:req.body.password})
+    .then((result)=>{
+        
+        res.json({message:"password changed",values:result})
+        
+    })
+    .catch((e)=>console.log(e))
+})
+
+
+
+app.post('/signIn', async(req, res) => {
+    
+   await db.collection("ast").findOne({email:req.body.email})
+    .then((result)=>{
+        if(result.password == req.body.password){
+            res.json({message:"login success",values:result})
+        }else{
+            res.json({error:"user not found"})
+        } 
+     }).catch((e)=>console.log(e))})
+
+
+app.post('/students', async(req, res) => {
+    
+    await db.collection("login").find().toArray()
+     .then((result)=>{
+         res.send(result)
+      }).catch((e)=>console.log(e))})
+
+      app.post('/signout',async(req,res)=>{
+        try{
+            req.logout();
+            res.json({message:"signed out successfully"});
+        }catch(error){
+            console.log(error);
+            res.json({error:"failed to  signout"})
+        }
+      })
+
+    connectToDB(() => {
+        app.listen(9000, () => {
+            console.log("server running at 9000");
+        })
+    })
